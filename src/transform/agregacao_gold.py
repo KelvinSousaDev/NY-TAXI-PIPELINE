ba02aca23550c0c326c3c743eff88e0bae3880db
KelvinSousaDev <kelvinsousa.dev@gmail.com>
2026-03-27 10:03:56 -0300

    feat(analytics): cria tabelas agregadas de negocio na camada gold

---
+++ src/transform/agregacao_gold.py
@@ -0,0 +1,23 @@
+import polars as pl
+from pathlib import Path
+
+caminho_silver = Path("data/silver")
+caminho_gold = Path("data/gold/")
+
+def agregar_dados_gold():
+    query_gold = (
+        pl.scan_parquet(caminho_silver/"*.parquet")
+        .group_by("PULocationID" as "Zona_Coleta")
+        .agg(
+            pl.col("fare_amount").sum().alias("receita_total"),
+            pl.len().alias("total_de_corridas")
+            )
+        .sort("receita_total", descending=True)
+        )
+    
+    df_gold = query_gold.collect()
+    df_gold.write_parquet(f"{caminho_gold}/faturamento_por_zona.parquet")
+    print(df_gold.head(5))
+
+if __name__ == "__main__":
+    agregar_dados_gold()
